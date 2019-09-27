f19354dc5c913eac90a63c06a1cf8b695b6ba4c4
Max Swank <ms2069@rit.edu>
2019-09-26 20:51:21 -0400

    Initial commit with code template

---
+++ regular.py
@@ -0,0 +1,44 @@
+"""
+regular.py
+it's anything bot
+"""
+
+import discord
+import praw
+import random
+
+BOT_TOKEN = 'NjI2OTM2ODY4MzM1Nzc5ODY1.XY1XKQ.jOauRwP_eE7njBuLwnGKvbc51h4'
+# reddit = praw.Reddit(
+#     client_id='',
+#     client_secret='',
+#     username='',
+#     password='',
+#     user_agent=''
+# )
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+        if message.content == '_ping':
+            await message.channel.send('pong')
+
+        # TODO Fix reddit bot
+        # if message.content.startswith('_meme'):
+        #     dankmemes = reddit.subreddit('dankmemes').hot(limit=100)
+        #     lst = []
+        #     for post in dankmemes:
+        #         lst.append(post.url)
+        #     memeurl = random.choice(lst)
+        #     await message.channel.send(memeurl)
+
+
+client = MyClient()
+client.run(BOT_TOKEN)
