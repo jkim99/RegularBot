3c55aaa7343c7d3607655dce2c7a08ff8d5652f7
jkim99 <jonathanskim@gmail.com>
2019-09-27 19:41:47 -0400

    fixed shit

--- regular.py
+++ regular.py
@@ -5,22 +5,21 @@ it's anything bot
 
 from datetime import datetime
 from reddit import REDDIT
-from spotify import SPOTIFY
+# from spotify import SPOTIFY
 from creds import BOT_TOKEN
 import discord
 
 
 PREFIX = '.'
-reddit = REDDIT()
 
 
 class MyClient(discord.Client):
     visited_posts = []
 
     async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
+        self.reddit = REDDIT()
+        print('Logged in as' + self.user.name)
+        print('User ID: ' + self.user.id)
 
     async def on_message(self, message):
         # we do not want the bot to reply to itself
@@ -32,7 +31,7 @@ class MyClient(discord.Client):
 
         # this adds the message to the log
         log = open('log.txt', 'a')
-        log.write('{}{} {}'.format(t, message.content, str(message.author)))
+        log.write('{}{} {}\n'.format(t, message.content, str(message.author)))
         log.close()
 
         # Ping --> Pong
@@ -40,21 +39,26 @@ class MyClient(discord.Client):
             await message.channel.send('pong!')
 
         # Closes bot
-        if message.content.startswith(PREFIX + 'stop'):
+        elif message.content.startswith(PREFIX + 'stop'):
             await message.channel.send('Stopping...')
             await self.logout()
 
         # Takes suggestions and writes it to a file
-        if message.content.startswith(PREFIX + 'suggestion'):
+        elif message.content.startswith(PREFIX + 'suggestion'):
             file = open('suggestions.txt', 'a')
             content = message.content.replace(PREFIX + 'suggestion', '')
             file.write('\n{}{} [{}]'.format(t, content, str(message.author)))
             file.close()
             await message.channel.send('Thank you for the suggestion')
 
-        # Invokes meme from the reddit module
-        if message.content.startswith(PREFIX + 'meme'):
-            await message.channel.send(reddit.meme(message))
+        # Gets meme from the reddit module
+        elif message.content.startswith(PREFIX + 'meme'):
+            await message.channel.send(self.reddit.meme(message))
+
+        # Plays music
+        elif message.content.startswith(PREFIX + 'play'):
+            channel = self.get_channel('General')
+            await discord.VoiceChannel.connect(channel)
 
 
 def main():
