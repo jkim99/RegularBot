e48a08937a82f6ff1c324f4d0ea0d6bf3206b6bd
jkim99 <jonathanskim@gmail.com>
2019-09-26 21:54:42 -0400

    test

--- regular.py
+++ regular.py
@@ -31,7 +31,7 @@ class MyClient(discord.Client):
         if message.content == '_ping':
             await message.channel.send('pong')
 
-        # this takes suggestions and writes it to a file
+        # This takes suggestions and writes it to a file
         if message.content.contains(PREFIX + "suggestions"):
             file = open("suggestions.txt", "w")
             content = message.content
