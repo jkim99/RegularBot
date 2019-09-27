7c8236efc775ffd90f814ed8b5bcf06e1d94d82b
Max Swank <ms2069@rit.edu>
2019-09-27 14:22:22 -0400

    re-coded Reddit changes and basic meme command

--- regular.py
+++ regular.py
@@ -5,21 +5,23 @@ it's anything bot
 
 from datetime import datetime
 import discord
-# import praw
+import praw
 # import random
 
 BOT_TOKEN = 'NjI2OTM2ODY4MzM1Nzc5ODY1.XY1xig.klUPz5__kSkqWxHAoFWs_8KnjKo'
-# reddit = praw.Reddit(
-#     client_id='',
-#     client_secret='',
-#     username='',
-#     password='',
-#     user_agent=''
-# )
+reddit = praw.Reddit(
+    client_id='',
+    client_secret='',
+    username='',
+    password='',
+    user_agent=''
+)
 PREFIX = '!'
 
 
 class MyClient(discord.Client):
+    visited_posts = []
+
     async def on_ready(self):
         print('Logged in as')
         print(self.user.name)
@@ -50,14 +52,13 @@ class MyClient(discord.Client):
             file.close()
             await message.channel.send('Thank you for the suggestion')
 
-        # TODO Fix reddit bot
-        # if message.content.startswith('_meme'):
-        #     dankmemes = reddit.subreddit('dankmemes').hot(limit=100)
-        #     lst = []
-        #     for post in dankmemes:
-        #         lst.append(post.url)
-        #     memeurl = random.choice(lst)
-        #     await message.channel.send(memeurl)
+        if message.content.startswith(PREFIX + 'meme'):
+            dankmemes = reddit.subreddit('dankmemes').hot()
+            for post in dankmemes:
+                if post.id not in self.visited_posts:
+                    await message.channel.send(post.url)
+                    self.visited_posts.append(post.id)
+                    break
 
 
 client = MyClient()
