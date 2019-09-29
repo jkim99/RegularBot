e2526549e5d94e0c23495389cd01346caeace42a
jkim99 <jonathanskim@gmail.com>
2019-09-28 22:49:48 -0400

    music bot doesn't work but it will someday

--- regular.py
+++ regular.py
@@ -5,11 +5,12 @@ it's anything bot
 
 from datetime import datetime
 from reddit import REDDIT
-# from spotify import SPOTIFY
 from creds import BOT_TOKEN
 from discord.ext import commands
 from discord import opus
 from discord import File
+# temp
+import discord
 import os
 
 
@@ -19,7 +20,6 @@ reddit = REDDIT()
 
 
 def log(message, author):
-    print(message)
     t = datetime.today().strftime('[%Y-%m-%d-%H:%M]')
     file = open('log.txt', 'a')
     file.write('\n{} {} [{}]'.format(t, str(message), str(author)))
@@ -55,9 +55,9 @@ async def suggestion(ctx):
     await ctx.send('Thank you for the suggestion!')
 
 
-# TODO: fix this shit
 @client.command(pass_context=True)
 async def meme(ctx):
+    log(ctx.message.content, ctx.message.author)
     filename = reddit.get_meme()
     if filename < 0:
         return
@@ -65,17 +65,20 @@ async def meme(ctx):
     os.remove(filename)
 
 
-# TODO: fix this shit too
+# TODO: fix this shit
 @client.command(pass_context=True)
 async def play(ctx, url):
-    channel = ctx.message.author.voice.channel
-    voice_client = await channel.connect()
-    player = await voice_client.create_ytdl_player(url)
-    player.start()
+    log(ctx.message.content, ctx.message.author)
+    voice = ctx.message.author.voice
+    if voice:
+        channel = voice.channel
+        voice_client = await channel.connect()
+        # voice_client.play(a, after=None)
 
 
 def main():
-    # opus.load_opus('opus')
+    if not opus.is_loaded():
+        opus.load_opus('opus')
     client.run(BOT_TOKEN)
 
 
