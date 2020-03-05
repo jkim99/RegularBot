026d8a8b167c7ed0a2f95e7fac1c9c14229aa66b
jk7783 <jk7783@rit.edu>
2020-03-04 23:53:27 -0500

    made progrss towards working que

--- regular.py
+++ regular.py
@@ -14,13 +14,13 @@ from discord.utils import get
 # from discord import opus
 from discord import File
 from discord import FFmpegPCMAudio as audio
+import asyncio
 import regular.config as config
 import os
 import random
 import requests
 
 
-PREFIX = '_'
 client = commands.Bot(command_prefix=config.PREFIX)
 reddit = REDDIT()
 youtube = YOUTUBE()
@@ -49,12 +49,13 @@ async def roll(ctx):
     log(ctx.message.content, ctx.message.author)
     result = int(random.random() * 100)
     await ctx.send('Rolling out of 100...')
-    await ctx.send(ctx.author.display_name + ' rolls a ' + str(result))
+    await ctx.send(f"{ctx.author.display_name} rolls a {str(result)}")
 
 
 @client.command(pass_context=True)
 async def shutdown(ctx):
     log(ctx.message.content, ctx.message.author)
+    youtube.clear_mp3()
     await ctx.send('Stopping...')
     await client.logout()
 
@@ -62,13 +63,20 @@ async def shutdown(ctx):
 @client.command(pass_context=True, aliases=['suggest', 'suggestions', 'sug'])
 async def suggestion(ctx):
     log(ctx.message.content, ctx.message.author)
+
+    cmd = ctx.message.content.split()[0].replace(config.PREFIX, "")
+
     t = datetime.today().strftime('[%Y-%m-%d-%H:%M]')
     file = open('suggestions.txt', 'a')
-    content = ctx.message.content.replace(config.PREFIX + 'suggestion', '')
+    content = ctx.message.content.replace(config.PREFIX + cmd, '')
     file.write('\n{}{} [{}]'.format(t, content, str(ctx.message.author)))
     file.close()
     await ctx.send('Thank you for the suggestion!')
 
+
+@client.command(pass_context=True, aliases=['lq'])
+async def listqueue(ctx):
+    await ctx.send(youtube.print_queue())
 
 @client.command(pass_context=True)
 async def meme(ctx, subreddit='dankmemes'):
@@ -80,30 +88,47 @@ async def meme(ctx, subreddit='dankmemes'):
     os.remove(filename)
 
 
-@client.command(pass_context=True)
-async def play(ctx, url=None):
+@client.command(pass_context=True, aliases=['play', 'que', 'q'])
+async def queue(ctx, url=None):
     log(ctx.message.content, ctx.message.author)
 
     # joining channel
-    channel = ctx.message.author.voice.channel
-    if not channel:
+    try:
+        channel = ctx.message.author.voice.channel
+        if not channel:
+            await ctx.send("You must be connected to a voice channel")
+            return
+        voice = get(client.voice_clients, guild=ctx.guild)
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+        else:
+            voice = await channel.connect()
+    except AttributeError:
         await ctx.send("You must be connected to a voice channel")
         return
-    voice = get(client.voice_clients, guild=ctx.guild)
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else:
-        voice = await channel.connect()
 
-    # queues url if it exists
+    # downloads and queues song if url exists
     if url:
         youtube.queue_song(url)
+    else:
+        await ctx.send(f"Usage: {config.PREFIX}play [youtube url]")
+        return
 
-    # downloads and plays the song
-    voice.play(audio(youtube.download()))
-    voice.volume = 100
-    voice.is_playing()
+    # plays the song
+    song = youtube.pop_queue()
+    voice.play(audio(song))
+    await ctx.send(f"Playing: {song}")
 
+
+@client.command(pass_context=True)
+async def skip(ctx):
+    log(ctx.message.content, ctx.message.author)
+    voice.stop()
+    await ctx.send("Skipping...")
+    song = youtube.pop_queue()
+    await ctx.send(f"Playing: {song}")
+    voice.play(audio(song))
+    
 
 @client.command(pass_context=True, aliases=['members'])
 async def clanmembers(ctx):
@@ -153,12 +178,6 @@ async def clanwar(ctx):
             await ctx.send('There is no war taking place right now.')
     else:
         await ctx.send('Response error')
-
-
-@client.command(pass_context=True, aliases=['que', 'q'])
-async def queue(ctx, url):
-    log(ctx.message.content, ctx.message.author)
-    youtube.queue_song(url)
 
 
 @client.command(pass_context=True)
